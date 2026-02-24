6e71c976ef156048a0ee227cc09d3adff30e07ce
gabrielthomsen06 <gabrielthomsen08@gmail.com>
2026-02-23 21:46:48 -0300

    Fix: corrige exibição da mensagem no Medidor de Humor

--- js/humor.js
+++ js/humor.js
@@ -1,4 +1,4 @@
-const form = document.querySelector("form");
+const form = document.getElementById("formHumor"); 
 const msgResultado = document.getElementById("msgResultado");
 
 form.addEventListener("submit", (e) => {
